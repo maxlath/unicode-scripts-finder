f5e169cb4df128ca88e17bc864d425534ccaf5bd
maxlath <k@maxlath.eu>
2020-07-18 14:49:45 +0200

    tests: add hybrid test

--- test/unicode_scripts_finder.js
+++ test/unicode_scripts_finder.js
@@ -7,15 +7,20 @@ describe('unicode-scripts-finder', () => {
   })
 
   it('should find Latin script', () => {
-    const res = unicodeScriptsFinder('hello')
-    res.should.deepEqual([
-      { label: 'Latin', count: 5 }
+    unicodeScriptsFinder('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.').should.deepEqual([
+      { label: 'Latin', count: 370 }
     ])
   })
 
   it('should find Greek script', () => {
-    const res = unicodeScriptsFinder('ἀπόστασις')
-    res.should.deepEqual([
+    unicodeScriptsFinder('ἀπόστασις').should.deepEqual([
+      { label: 'Greek', count: 9 }
+    ])
+  })
+
+  it('should find hybrid script', () => {
+    unicodeScriptsFinder('Lorem ipsum ἀπόστασις').should.deepEqual([
+      { label: 'Latin', count: 10 },
       { label: 'Greek', count: 9 }
     ])
   })
